2057db481daf43417cff798bd091bb1b30b45611
Prasanth A R <raviprasanth45@gmail.com>
2021-02-19 14:54:29 +0530

    Added Header Component

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from 'reactstrap';
 
-export default function HeaderComponent() {
-    return (
-        <div>
-            
-        </div>
-    )
+const Example = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/"><i>You.Do</i></NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav navbar>
+            <NavItem>
+            <NavLink href="https://github.com/ptech12/you.do"> <i class="fa fa-github-square" aria-hidden="true"></i> GitHub</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
 }
+
+export default Example;
+
