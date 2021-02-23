a478a007315be94c546a97f6fc40e61903038a0c
Prasanth A R <raviprasanth45@gmail.com>
2021-02-23 22:15:55 +0530

    Added some Component, Getting some ideas, Please Contribute any one

---
+++ src/components/AddTaskComponent.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+import { Row, Col, Button, InputGroup, FormControl, ListGroup } from "reactstrap";
+
+
+export default class AddTaskComponent extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
